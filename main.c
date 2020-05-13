32c6e7977270d5af249b0e3aa50ac464e2371a5a
stephench420 <benjamin.lesieux@efrei.net>
2020-05-13 19:39:37 +0200

    Remis les structures

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 
 #include "LinkedList.h"
 
-
 int main() {
 
+    xmlDocPtr doc = xmlParseFile("dbbooks.xml");
+    xmlNodePtr node;
 
+    xmlKeepBlanksDefault(0);
+
+    node = xmlDocGetRootElement(doc);
 
     return 0;
 }
